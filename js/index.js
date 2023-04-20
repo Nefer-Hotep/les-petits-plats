001d0af1b460f0c2e608da0883d5ebfee1fc8fe3
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2023-04-20 17:37:42 +0200

    fix: recipe card list

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
 // Gère l'affichage des menu dropdown
-function dropdownDisplay() {
-    document.addEventListener('click', (e) => {
-        const isDropdownInput = e.target.matches('[data-dropdown-input]');
+document.addEventListener('click', (e) => {
+    const isDropdownInput = e.target.matches('[data-dropdown-input]');
 
-        if (!isDropdownInput && e.target.closest('[data-dropdown]') != null)
-            return;
+    if (!isDropdownInput && e.target.closest('[data-dropdown]') != null) return;
 
-        let currentDropdown;
-        if (isDropdownInput) {
-            currentDropdown = e.target.closest('[data-dropdown]');
-            currentDropdown.classList.toggle('active');
-        }
+    let currentDropdown;
+    if (isDropdownInput) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
 
-        document
-            .querySelectorAll('[data-dropdown].active')
-            .forEach((dropdown) => {
-                if (dropdown === currentDropdown) return;
-                dropdown.classList.remove('active');
-            });
+    document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
     });
-}
+});
 
 const recipeCardTemplate = document.querySelector('[data-recipe-template]');
 const listTemplate = document.querySelector('[data-list-template]');
@@ -27,25 +22,46 @@ const recipeCardContainer = document.querySelector(
     '[data-recipe-card-container]'
 );
 
-// Récupère les données de l'API
+let recipes = [];
+
 fetch('../data/recipes.js')
     .then((res) => res.json())
     .then((data) => {
-        data.forEach((recipe) => {
-            const listContainer = document.querySelector(
-                '[data-list-container]'
-            );
-
-            const card = recipeCardTemplate.content.cloneNode(true);
+        recipes = data.map((recipe) => {
+            const card = recipeCardTemplate.content.cloneNode(true).children[0];
             const name = card.querySelector('[data-name]');
             const time = card.querySelector('[data-time]');
             const description = card.querySelector('[data-description]');
+            const ingredientContainer = card.querySelector(
+                '[data-list-container]'
+            );
 
+            // Display ingredients inside the card
             recipe.ingredients.forEach((ingredient) => {
                 const list = listTemplate.content.cloneNode(true);
-                const ingredients = card.querySelector('[data-ingredients]');
+                const ingredientElement =
+                    list.querySelector('[data-ingredient]');
+                // const quantityElement = list.querySelector('[data-quantity]');
+                ingredientElement.textContent =
+                    ingredient.ingredient +
+                    (!!ingredient.quantity ? ' : ' : ' ');
+                // quantityElement.textContent = ingredient.quantity;
 
-                // listContainer.append(list);
+                if (ingredient.quantity) {
+                    const quantityElement = document.createElement('span');
+                    quantityElement.setAttribute(
+                        'class',
+                        'card__recipe-list-quantity'
+                    );
+                    quantityElement.textContent =
+                        ingredient.quantity +
+                        ' ' +
+                        (!!ingredient.unit ? ingredient.unit : '');
+                    ingredientElement.appendChild(quantityElement);
+                }
+
+                // console.log(ingredientElement);
+                ingredientContainer.append(ingredientElement);
             });
 
             name.textContent = recipe.name;
@@ -53,18 +69,42 @@ fetch('../data/recipes.js')
             description.textContent = recipe.description;
 
             recipeCardContainer.append(card);
+
+            return {
+                name: recipe.name,
+                ingredients: recipe.ingredients,
+                element: card,
+            };
         });
     })
-    .catch((err) => console.log('an error occurs' + err));
+    .catch((err) => console.error('an error occurs ' + err));
 
 const searchInput = document.querySelector('[data-search]');
 
 searchInput.addEventListener('input', (e) => {
     const value = e.target.value.toLowerCase();
 
-    if (value.length > 2) {
-        recipes.forEach((recipe) => {
-            const isVisible = recipe.name.toLowerCase().includes(value);
-        });
-    }
+    // if (value.length > 3) {
+    recipes.forEach((recipe) => {
+        const isVisible =
+            recipe.name.toLowerCase().includes(value) ||
+            recipe.ingredients.filter((obj) =>
+           { console.log(obj)
+                obj.ingredient.toLowerCase().includes(value)}
+            );
+
+        recipe.element.classList.toggle('hide', !isVisible);
+    });
+    // } else {
+    //     recipes.forEach((recipe) => {
+    //         const isVisible =
+    //             recipe.name.toLowerCase().includes(value) ||
+    //             recipe.ingredients.some((obj) => {
+    //                 console.log("else");
+    //                 obj.ingredient.toLowerCase().includes(value);
+    //             });
+
+    //         recipe.element.classList.toggle('hide', !isVisible);
+    //     });
+    // }
 });
